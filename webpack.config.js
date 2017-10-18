1c45b3a3031c4f7eb8b98f22cf02d5c062b93576
Matt Martin <08martinm@gmail.com>
2017-10-17 18:57:56 -0700

    silencing console outputs

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,27 @@ const env = require('./config/env.js');
 
 process.env.BABEL_ENV === env ? 'development' : 'production';
 
+let stats = !env ? 'normal' :
+  {
+    hash: false,
+    version: false,
+    timings: false,
+    assets: false,
+    chunks: false,
+    modules: false,
+    reasons: false,
+    children: false,
+    source: false,
+    errors: false,
+    errorDetails: false,
+    warnings: false,
+    publicPath: false,
+  };
+
 module.exports = {
   entry: parts.entry,
   output: parts.output,
+  stats: stats,
 
   plugins: parts.plugins,
   devtool: parts.devtool,

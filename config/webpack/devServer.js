d08e477ee9e8cd2b5b439842ce755aed2c49960b
Matt Martin <08martinm@gmail.com>
2017-10-17 18:50:01 -0700

    updated webpack config to silence output

--- config/webpack/devServer.js
+++ config/webpack/devServer.js
@@ -1,11 +1,28 @@
 const path = require('path');
 const env = require('../env.js');
 
+let stats = !env ? 'normal' :
+  {
+    hash: false,
+    version: false,
+    timings: false,
+    assets: false,
+    chunks: false,
+    modules: false,
+    reasons: false,
+    children: false,
+    source: false,
+    errors: false,
+    errorDetails: false,
+    warnings: false,
+    publicPath: false,
+  };
+
 exports.devServer =
   { 
     contentBase: path.join(__dirname, '../../public'),
     historyApiFallback: true,
-    stats: !env ? 'normal' : {warnings: false},
+    stats: stats,
     hot: env,
     overlay: {
       errors: env,
